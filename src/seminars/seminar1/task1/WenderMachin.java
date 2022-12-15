937b79ee287976075f9b90b243b7723f95f5c15a
Sh1rsh1n <a.s.shirshin@mail.ru>
2022-12-15 20:19:55 +0300

    inti commit #1

---
+++ src/seminars/seminar1/task1/WenderMachin.java
@@ -0,0 +1,14 @@
+package seminars.seminar1.task1;
+
+import java.util.List;
+
+abstract public class WenderMachin {
+
+    public abstract int enterNumber();
+
+    public abstract Goods showGoods(int productNumber);
+
+    public abstract void goodsBuy(Goods goods);
+
+
+}
